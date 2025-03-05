b39f4e9d8f93c94679d0346e7a126857803db82d
Lucas Gervasoni <jaxellucas@gmail.com>
2025-03-05 20:24:58 -0300

    creating a json for stacks

--- src/components/Tech/index.jsx
+++ src/components/Tech/index.jsx
@@ -1,5 +1,6 @@
 import "@/components/Tech/AboutTech.css";
 import { ProjectsList } from "@/components/ProjectsList/index";
+import stack from "@/data/stack.json";
 
 export function AboutTech() {
   return (
@@ -25,98 +26,13 @@ export function AboutTech() {
       <div className="aboutTechContent flex flex-col gap-10">
         <h2 className="text-3xl font-bold tracking-widest">Tech Stack</h2>
         <div className="flex gap-2 flex-wrap justify-center items-center">
-          <img
-            alt="html"
-            src="https://img.shields.io/badge/HTML-black?style=for-the-badge&logo=html5&logoColor=white"
+          {stack.map((stack, index) => (
+            <img
+            key={index}
+            alt={stack.name}
+            src={stack.url}
           />
-          <img
-            alt="css"
-            src="https://img.shields.io/badge/CSS-black?style=for-the-badge&logo=CSS&logoColor=white"
-          />
-          <img
-            alt="sass"
-            src="https://img.shields.io/badge/SASS-black?style=for-the-badge&logo=sass&logoColor=white"
-          />
-          <img
-            alt="javascript"
-            src="https://img.shields.io/badge/JavaScript-black?style=for-the-badge&logo=javascript&logoColor=white"
-          />
-          <img
-            alt="node.js"
-            src="https://img.shields.io/badge/node.js-black?style=for-the-badge&logo=node.js&logoColor=white"
-          />
-          <img
-            alt="Express.js"
-            src="https://img.shields.io/badge/Express.js-black?style=for-the-badge&logo=express&logoColor=white"
-          />
-          <img
-            alt="nestjs"
-            src="https://img.shields.io/badge/nestjs-black?style=for-the-badge&logo=nestjs&logoColor=white"
-          />
-          <img
-            alt="Next.js"
-            src="https://img.shields.io/badge/next%20js-black?style=for-the-badge&logo=nextdotjs&logoColor=white"
-          />
-          <img
-            alt="React.js"
-            src="https://img.shields.io/badge/React%20js-black?style=for-the-badge&logo=react&logoColor=white"
-          />
-          <img
-            alt="Typescript"
-            src="https://img.shields.io/badge/TypeScript-black?style=for-the-badge&logo=typescript&logoColor=white"
-          />
-          <img
-            alt="Tailwind"
-            src="https://img.shields.io/badge/Tailwind-black?style=for-the-badge&logo=tailwind-css&logoColor=white"
-          />
-          <img
-            alt="Bootstrap"
-            src="https://img.shields.io/badge/Bootstrap-black?style=for-the-badge&logo=bootstrap-css&logoColor=white"
-          />
-          <img
-            alt="Java"
-            src="https://img.shields.io/badge/Java-black?style=for-the-badge&logo=openjdk&logoColor=white"
-          />
-          <img
-            alt="springboot"
-            src="https://img.shields.io/badge/springboot-black?style=for-the-badge&logo=springboot&logoColor=white"
-          />
-          <img
-            alt="Python"
-            src="https://img.shields.io/badge/Python-black?style=for-the-badge&logo=python&logoColor=white"
-          />
-          <img
-            alt="Django"
-            src="https://img.shields.io/badge/Django-black?style=for-the-badge&logo=django&logoColor=white"
-          />
-          <img
-            alt="mysql"
-            src="https://img.shields.io/badge/mysql-black?style=for-the-badge&logo=mysql&logoColor=white"
-          />
-          <img
-            alt="postgresql"
-            src="https://img.shields.io/badge/postgresql-black?style=for-the-badge&logo=postgresql&logoColor=white"
-          />
-          <img
-            alt="git"
-            src="https://img.shields.io/badge/Git-black?style=for-the-badge&logo=git&logoColor=white"
-          />
-          <img
-            alt="aws"
-            src="https://img.shields.io/badge/AWS-black?style=for-the-badge&logo=amazon-web-services&logoColor=white"
-          />
-          <img
-            alt="heroku"
-            src="https://img.shields.io/badge/Heroku-black?style=for-the-badge&logo=heroku&logoColor=white"
-          />
-          <img
-            alt="vercel"
-            src="https://img.shields.io/badge/Vercel-black?style=for-the-badge&logo=vercel&logoColor=white"
-          />
-          <img
-            alt="figma"
-            src="https://img.shields.io/badge/Figma-black?style=for-the-badge&logo=figma&logoColor=white"
-          />
+          ))}
         </div>
       </div>
       <div className="aboutTechContent flex flex-col gap-10">
